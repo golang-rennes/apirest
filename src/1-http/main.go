56ca9e059dd09dd5a258ea80572497d0551d6dc0
Benoit Masson <benoit@open-agora.com>
2017-06-12 17:38:51 +0200

    [1-HTTP] Ajout du handler GET /users
    => filtre selon la méthode et retourne GetAll() si GET

--- src/1-http/main.go
+++ src/1-http/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"data"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,9 +14,29 @@ const (
 )
 
 // time repeat.sh 1000 curl -i -X GET localhost:8888/users >& /dev/null
+// => < 5ms
 
 func main() {
 	// curl -i -X GET localhost:8888/users && echo
+	http.HandleFunc("/users", func(w http.ResponseWriter, req *http.Request) {
+		// request filtering
+		if req.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		// get users
+		allUsers := data.GetAll()
+
+		// return users
+		js, err := json.Marshal(allUsers)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	})
 
 	server := fmt.Sprintf("%s:%d", host, port)
 	log.Printf("Serving API on http://%s…", server)
