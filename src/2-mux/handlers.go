b73e98a3a104a394de60780d1310196adda7cac0
Benoit Masson <benoit@open-agora.com>
2017-06-13 15:48:23 +0200

    [2-Mux] Regexp sur param entier

--- src/2-mux/handlers.go
+++ src/2-mux/handlers.go
@@ -20,8 +20,8 @@ func listUsers(w http.ResponseWriter, req *http.Request) {
 func getUser(w http.ResponseWriter, req *http.Request) {
 	// get ID from path
 	vars := mux.Vars(req)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil || id < 0 {
+	id, _ := strconv.Atoi(vars["id"])
+	if id < 0 {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte(fmt.Sprintf("Invalid id \"%s\"", vars["id"])))
 		return
--- src/2-mux/main.go
+++ src/2-mux/main.go
@@ -14,7 +14,9 @@ const (
 )
 
 // time repeat.sh 1000 curl -i -X GET localhost:8888/users >& /dev/null
+// => < 5ms
 // time repeat.sh 1000 curl -i -X GET localhost:8888/users/1789 >& /dev/null
+// => < 5ms
 
 func main() {
 	r := mux.NewRouter()
@@ -24,7 +26,7 @@ func main() {
 	// curl -i -X GET localhost:8888/users/1789 && echo
 	// curl -i -X GET localhost:8888/users/2017 && echo
 	// curl -i -X GET localhost:8888/users/2020 && echo
-	r.HandleFunc("/users/{id}", getUser).Methods(http.MethodGet)
+	r.HandleFunc("/users/{id:[0-9]+}", getUser).Methods(http.MethodGet)
 
 	// curl -i -X POST localhost:8888/users -d '{"firstname":"toto", "lastname":"titi"}' && echo
 
