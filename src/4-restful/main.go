f128baae06bf8017398fa12659a0973e80a59ce0
Benoit Masson <benoit@open-agora.com>
2017-06-14 21:42:05 +0200

    [4-Restful] Mise en place de Swagger-UI
    + service apidocs.json

--- src/4-restful/main.go
+++ src/4-restful/main.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 
 	restful "github.com/emicklei/go-restful"
+	openapi "github.com/emicklei/go-restful-openapi"
 )
 
 const (
-	host = "localhost"
-	port = 8888
+	host       = "localhost"
+	port       = 8888
+	swaggerDir = "./swagger-ui-2"
 )
 
 // time repeat.sh 1000 curl -i -X GET localhost:8888/users >& /dev/null
@@ -19,6 +21,8 @@ const (
 // => 6ms
 
 func main() {
+	server := fmt.Sprintf("%s:%d", host, port)
+
 	// initialize go-restful container
 	wsContainer := restful.NewContainer()
 
@@ -26,9 +30,18 @@ func main() {
 	userRsc := &userResource{}
 	userRsc.register(wsContainer)
 
+	// initialize OpenAPI file and serve Swagger-UI on /docs
+	config := openapi.Config{
+		WebServices:    wsContainer.RegisteredWebServices(),
+		WebServicesURL: "http://" + server,
+		APIPath:        "/apidocs.json",
+	}
+	wsContainer.Add(openapi.NewOpenAPIService(config))
+	http.Handle("/docs/", http.StripPrefix("/docs", http.FileServer(http.Dir(swaggerDir+"/dist"))))
+	log.Printf("Serving Swagger-UI on http://%s/docs…", server)
+
 	// run
 	http.Handle("/", wsContainer)
-	server := fmt.Sprintf("%s:%d", host, port)
-	log.Printf("Serving API on http://%s…", server)
+	log.Printf("Serving API on http://%s and\n                   apidocs.json on http://%s/apidocs.json…", server, server)
 	log.Fatal(http.ListenAndServe(server, nil))
 }
--- src/4-restful/resource.go
+++ src/4-restful/resource.go
@@ -1,6 +1,9 @@
 package main
 
-import restful "github.com/emicklei/go-restful"
+import (
+	restful "github.com/emicklei/go-restful"
+	openapi "github.com/emicklei/go-restful-openapi"
+)
 
 type userResource struct{}
 
@@ -12,15 +15,20 @@ func (u *userResource) register(container *restful.Container) {
 		Consumes(restful.MIME_JSON).
 		Produces(restful.MIME_JSON)
 
+	tags := []string{"users"}
+
 	// define routes
 	// curl -i -X GET localhost:8888/users && echo
-	ws.Route(ws.GET("/").To(findUsers))
+	ws.Route(ws.GET("/").To(findUsers).
+		Metadata(openapi.KeyOpenAPITags, tags))
 
 	// curl -i -X GET localhost:8888/users/1789 && echo
-	ws.Route(ws.GET("/{id}").To(getUser))
+	ws.Route(ws.GET("/{id}").To(getUser).
+		Metadata(openapi.KeyOpenAPITags, tags))
 
 	// curl -i -X POST localhost:8888/users -d '{"firstname":"toto", "lastname":"titi"}' && echo
-	ws.Route(ws.POST("/").To(createUser))
+	ws.Route(ws.POST("/").To(createUser).
+		Metadata(openapi.KeyOpenAPITags, tags))
 
 	container.Add(ws)
 }
