47d64287ddb32118838b0806ab2f47a14d4aa71d
Benoit Masson <benoit@open-agora.com>
2017-06-14 22:01:33 +0200

    [4-Restful] Documentation complète

--- src/4-restful/resource.go
+++ src/4-restful/resource.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"data"
+	"net/http"
+
 	restful "github.com/emicklei/go-restful"
 	openapi "github.com/emicklei/go-restful-openapi"
 )
@@ -20,15 +23,28 @@ func (u *userResource) register(container *restful.Container) {
 	// define routes
 	// curl -i -X GET localhost:8888/users && echo
 	ws.Route(ws.GET("/").To(findUsers).
-		Metadata(openapi.KeyOpenAPITags, tags))
+		Metadata(openapi.KeyOpenAPITags, tags).
+		Doc("gets all users list").
+		Param(ws.QueryParameter("firstname", "filter users with this firstname").DataType("string").Required(false)).
+		Writes([]data.User{}).
+		Returns(http.StatusOK, "success, users list is returned", []data.User{}))
 
 	// curl -i -X GET localhost:8888/users/1789 && echo
 	ws.Route(ws.GET("/{id}").To(getUser).
-		Metadata(openapi.KeyOpenAPITags, tags))
+		Metadata(openapi.KeyOpenAPITags, tags).
+		Doc("gets a specific user").
+		Param(ws.PathParameter("id", "identifier of the user").DataType("integer").Required(true)).
+		Writes(data.User{}).
+		Returns(http.StatusOK, "success, user is returned", data.User{}).
+		Returns(http.StatusNotFound, "user does not exist", nil))
 
 	// curl -i -X POST localhost:8888/users -d '{"firstname":"toto", "lastname":"titi"}' && echo
 	ws.Route(ws.POST("/").To(createUser).
-		Metadata(openapi.KeyOpenAPITags, tags))
+		Metadata(openapi.KeyOpenAPITags, tags).
+		Doc("creates a user").
+		Reads(data.User{}).
+		Writes(data.User{}).
+		Returns(http.StatusOK, "success, created user is returned", data.User{}))
 
 	container.Add(ws)
 }
